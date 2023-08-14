aa81494a9c57a4458c5941b351daed75f1f33ccc
twallema <tijs.alleman@ugent.be>
2023-08-14 13:39:29 +0200

    function to get data

--- src/pyIEEM/data/data.py
+++ src/pyIEEM/data/data.py
@@ -1,8 +1,51 @@
 import os
+import numpy as np
 import pandas as pd
 from datetime import datetime
 
 abs_dir = os.path.dirname(__file__)
+
+def get_economic_data(indicator, country):
+    """
+    A function to load and format the GDP or employment data of country `country`
+
+    input
+    =====
+
+    indicator: str
+        economic indicator. 'GDP' or 'employment'.
+        
+    country: str
+        country name. 'BE' or 'SWE'
+
+    output
+    ======
+
+    data: pd.Series
+        GDP or employment data
+    """
+
+    # input check
+    assert ((indicator == 'GDP') | (indicator == 'employment')), f"invalid economic indicator '{indicator}'. valid indicators are 'GDP' and 'employment'"
+    assert ((country=='BE') | (country=='SWE')), f"invalid country name '{country}'. valid names are 'BE' and 'SWE'."
+
+    # load data
+    data = pd.read_csv(os.path.join(
+        abs_dir, f'../../../data/interim/eco/calibration_data/{country}.csv'), index_col=[0], parse_dates=True)[indicator].dropna()
+
+    # convert to absolute data
+    other_accounts = pd.read_csv(os.path.join(abs_dir, f"../../../data/interim/eco/national_accounts/{country}/other_accounts_{country}_NACE64.csv"), sep=',',header=[0],index_col=[0])
+    if country == 'SWE':
+        curr = '(Mkr/y)'
+    else:
+        curr = '(M€/y)'
+
+    if indicator == 'GDP':
+        data *= sum(np.array(other_accounts['Sectoral output ' + curr].values)/365)
+    else:
+        data *= sum(np.array(other_accounts['Labor compensation ' + curr].values)/365)
+
+    return data
 
 def get_hospitalisation_incidence(country, aggregate_bxl_brabant=False):
     """
