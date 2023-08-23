09eeadedb11a9551d3ac54ab1446828d888f2f91
Tijs Alleman <tijs.alleman@ugent.be>
2023-08-23 11:15:01 +0200

    increase pi_work

--- notebooks/calibrate-epinomic-models.py
+++ notebooks/calibrate-epinomic-models.py
@@ -84,10 +84,10 @@ labels = [r'$\nu$', r'$\xi_{eff}$', r'$\pi_{eff}$', r'$\pi_{work}$', r'$\pi_{lei
 log_prior_prob_fnc=[log_prior_normal_L2, log_prior_normal_L2, log_prior_normal_L2, log_prior_normal_L2, log_prior_normal_L2, log_prior_normal_L2, log_prior_normal_L2, log_prior_normal_L2,
                         log_prior_normal_L2, log_prior_normal_L2, log_prior_normal_L2, log_prior_normal_L2]
 
-theta = [22, 0.45, 0.07, 0.025, 0.06, 1, 0.20, -7, 0.20, 14, 7, 7] # where are my parameters?  
-mu_list = [22, 0.45, 0.07, 0.025, 0.06, 1, 0.20, 0, 0.20, 0, 7, 7] # where do I expect the parameters to be?
-sigma_list = [1, 0.03, 0.005, 0.0025, 0.005, 0.1, 0.02, 7, 0.03, 7, 2, 2] # How much noise do I expect there to be on the parameter value?
-l_list = [8, 10, 20, 20, 15, 10, 20, 16, 20, 14, 10, 10] # How strong are my beliefs?
+theta = [24, 0.45, 0.07, 0.04, 0.06, 1, 0.20, -7, 0.20, 14, 7, 7] # where are my parameters?  
+mu_list = [22, 0.45, 0.07, 0.04, 0.06, 1, 0.20, 0, 0.20, 0, 7, 7] # where do I expect the parameters to be?
+sigma_list = [1, 0.03, 0.005, 0.005, 0.005, 0.1, 0.02, 7, 0.03, 7, 2, 2] # How much noise do I expect there to be on the parameter value?
+l_list = [8, 10, 25, 25, 15, 10, 20, 16, 20, 14, 10, 10] # How strong are my beliefs?
 log_prior_prob_fnc_args=[]
 for mu,sigma,l in zip(mu_list,sigma_list, l_list):
     log_prior_prob_fnc_args += [(mu, sigma, l),]
